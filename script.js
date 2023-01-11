52e5cea4da9b07dfb2a1bb7672a7ab312d2976a0
EZillman <evelina.zillman@medieinstitutet.se>
2023-01-11 17:43:27 +0100

    Klar med styling, påbörjat js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 import './style/style.scss';
+
+// Om möjligt, lägg alla variabler längst upp, så har vi ordning direkt
+console.log('hej');
+
+const checkBtn = document.querySelector('#checkBtn');
+const menu = document.querySelector('#menu');
+
+function toggleMenu() {
+    if (checkbox.checked) {
+        menu.classList.toggle('showMenu');
+    }
+}
+
+
+checkBtn.addEventListener('change', toggleMenu); 
+
+
