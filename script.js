682b6814bfbdcaca8118c41c71e4b106c81b9a76
EZillman <evelina.zillman@medieinstitutet.se>
2023-01-12 13:24:28 +0100

    nu kommer man direkt till länken från menyn

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-// import './style/style.scss';
-
-// Om möjligt, lägg alla variabler längst upp, så har vi ordning direkt
-
 
 const checkBtn = document.querySelector('#checkBtn');
+const hamburger = document.querySelector('#hamburger');
 const menu = document.querySelector('#menu');
+const aboutLink = document.querySelector('#aboutLink');
+const ticketsLink = document.querySelector('#ticketsLink');
 
 
 function toggleMenu() {
     menu.classList.toggle('showMenu');
+    hamburger.classList.toggle('fa-xmark');
 }
+
+// TODO: Dem här två funktionerna innehåller samma sak,
+// går det att skriva ihop på något sätt?
+function goToSection2() {
+    menu.classList.toggle('showMenu');
+    hamburger.classList.toggle('fa-xmark');
+}
+
+function goToSection3() {
+    menu.classList.toggle('showMenu');
+    hamburger.classList.toggle('fa-xmark');
+} 
 
 
 checkBtn.addEventListener('change', toggleMenu); 
+aboutLink.addEventListener('click', goToSection2);
+ticketsLink.addEventListener('click', goToSection3);
 
 
 
