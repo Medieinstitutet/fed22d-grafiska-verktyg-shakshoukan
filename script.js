fe9c5e6aa496c2516bc92e8ec51f5882ea63e951
EZillman <evelina.zillman@medieinstitutet.se>
2023-01-19 14:25:21 +0100

    knappar används för att scrolla mellan biljetter

--- script.js
+++ script.js
@@ -8,8 +8,7 @@ const header = document.querySelector('#header');
 /*const ticket1 = document.querySelector('#ticket1');
 const ticket2 = document.querySelector('#ticket2');
 const ticket3 = document.querySelector('#ticket3');
-const prevBtn = document.querySelector('#prevBtn');
-const nextBtn = document.querySelector('#nextBtn');
+
 
 const tickets = [
     ticket1,
@@ -17,7 +16,8 @@ const tickets = [
     ticket3
 ]; */
 
-
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
 
 const rejectCookiesButton = document.querySelector('.reject-button');
 const acceptCookiesButton = document.querySelector('.allow-button');
@@ -37,13 +37,13 @@ function toggleMenu() {
     menu.classList.toggle('showMenu');
     hamburger.classList.toggle('fa-xmark');
     hamburger.classList.toggle('fa-bars');
-}
+};
 
 function goToSectionX() {
     menu.classList.toggle('showMenu');
     hamburger.classList.toggle('fa-xmark');
     hamburger.classList.toggle('fa-bars');
-}
+};
 
 window.onscroll = function() {
     let currentScroll = document.documentElement.scrollTop;
@@ -53,7 +53,7 @@ window.onscroll = function() {
         header.classList.add('hide');
     }
     prevScroll = currentScroll;
-}
+};
 
 /*function changeOpacity() {
     opacity -= 10;
@@ -116,18 +116,27 @@ function init() {
 } */
 
 
+function prevTicket() {
+    document.getElementById('ticketCards').scrollLeft += -400;
+};
+
+function nextTicket() {
+    document.getElementById('ticketCards').scrollLeft += 400;
+};
+   
+
+
 function disableCookies() {
   cookiesContainer.remove();
-}
+};
 
 
 checkBtn.addEventListener('change', toggleMenu); 
 aboutLink.addEventListener('click', goToSectionX);
 ticketsLink.addEventListener('click', goToSectionX);
 
-//prevBtn.addEventListener('click', prevTicket);
-//nextBtn.addEventListener('click', nextTicket);
-
+prevBtn.addEventListener('click', prevTicket);
+nextBtn.addEventListener('click', nextTicket);
 
 rejectCookiesButton.addEventListener('click', disableCookies);
 acceptCookiesButton.addEventListener('click', disableCookies);
